7f3ccf79d1d6301b0fca7d214dd7332f358903bd
Adam Kielar <adam@adamkielar.pl>
2020-02-25 23:11:25 +0100

    updating packages second attempt

--- minerals_catalog/urls.py
+++ minerals_catalog/urls.py
@@ -24,4 +24,10 @@ urlpatterns = [
     path('admin/', admin.site.urls),
 ]
 
+if settings.DEBUG:
+    import debug_toolbar
+    urlpatterns += [
+        path('__debug__/', include(debug_toolbar.urls, namespace='djdt')),
+    ]
+
 urlpatterns += staticfiles_urlpatterns()
