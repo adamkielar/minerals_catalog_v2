47fde68faf985fbc1e58b2c73bd231ebd9c8e3fe
Adam Kielar <adam@adamkielar.pl>
2020-03-10 21:35:52 +0100

    updated README and requirements.txt files

--- README.md
+++ README.md
@@ -17,12 +17,7 @@ In this project I have two views:
 * View with List of all minerals from database and footer with link to random mineral
 * View with mineral detail page
 
-### Used packages
-
-asgiref==3.2.3
-Django==3.0.2
-django-extensions==2.2.6
-Pillow==7.0.0
-pytz==2019.3
-six==1.14.0
-sqlparse==0.3.0
+In this project i have 3 filters:
+* user can search for mineral, search filter will check all model fields
+* user can search for mineral by group and category with dropdown filter
+* user can search for mineral by glossary filter
--- minerals/views.py
+++ minerals/views.py
@@ -8,58 +8,67 @@ def mineral_list(request):
     minerals = Mineral.objects.all()
     mineral_filter = MineralFilter(request.GET, queryset=minerals)
     minerals = mineral_filter.qs
-    active_letter = 'A'
+    active_letter = "A"
 
-    return render(request, 'minerals/minerals_list.html', {'minerals': minerals,
-                                                           'mineral_filter': mineral_filter,
-                                                           'active_letter': active_letter
-                                                           })
+    return render(
+        request,
+        "minerals/minerals_list.html",
+        {
+            "minerals": minerals,
+            "mineral_filter": mineral_filter,
+            "active_letter": active_letter,
+        },
+    )
 
 
 def mineral_detail(request, pk):
     mineral = get_object_or_404(Mineral, pk=pk)
     mineral_values = Mineral.objects.values().filter(pk=pk).first()
 
-    return render(request, 'minerals/mineral_detail.html', {'mineral': mineral,
-                                                            'mineral_values': mineral_values,
-                                                            })
+    return render(
+        request,
+        "minerals/mineral_detail.html",
+        {"mineral": mineral, "mineral_values": mineral_values,},
+    )
 
 
-def mineral_letter(request, letter='A'):
+def mineral_letter(request, letter="A"):
     minerals = Mineral.objects.filter(Q(name__istartswith=letter))
 
     active_letter = letter
 
-    return render(request, 'minerals/minerals_list.html', {'minerals': minerals,
-                                                           'active_letter': letter,
-                                                           })
+    return render(
+        request,
+        "minerals/minerals_list.html",
+        {"minerals": minerals, "active_letter": letter,},
+    )
 
 
 def mineral_search(request):
-    query = request.GET.get('q')
+    query = request.GET.get("q")
 
     if query:
         minerals = Mineral.objects.filter(
-            Q(name__icontains=query) |
-            Q(image_filename__icontains=query) |
-            Q(image_caption__icontains=query) |
-            Q(category__icontains=query) |
-            Q(formula__icontains=query) |
-            Q(strunz_classification__icontains=query) |
-            Q(crystal_system__icontains=query) |
-            Q(unit_cell__icontains=query) |
-            Q(color__icontains=query) |
-            Q(crystal_symmetry__icontains=query) |
-            Q(cleavage__icontains=query) |
-            Q(mohs_scale_hardness__icontains=query) |
-            Q(luster__icontains=query) |
-            Q(streak__icontains=query) |
-            Q(diaphaneity__icontains=query) |
-            Q(optical_properties__icontains=query) |
-            Q(refractive_index__icontains=query) |
-            Q(group__icontains=query) |
-            Q(crystal_habit__icontains=query) |
-            Q(specific_gravity__icontains=query)
+            Q(name__icontains=query)
+            | Q(image_filename__icontains=query)
+            | Q(image_caption__icontains=query)
+            | Q(category__icontains=query)
+            | Q(formula__icontains=query)
+            | Q(strunz_classification__icontains=query)
+            | Q(crystal_system__icontains=query)
+            | Q(unit_cell__icontains=query)
+            | Q(color__icontains=query)
+            | Q(crystal_symmetry__icontains=query)
+            | Q(cleavage__icontains=query)
+            | Q(mohs_scale_hardness__icontains=query)
+            | Q(luster__icontains=query)
+            | Q(streak__icontains=query)
+            | Q(diaphaneity__icontains=query)
+            | Q(optical_properties__icontains=query)
+            | Q(refractive_index__icontains=query)
+            | Q(group__icontains=query)
+            | Q(crystal_habit__icontains=query)
+            | Q(specific_gravity__icontains=query)
         )
 
-    return render(request, 'minerals/minerals_list.html', {'minerals': minerals})
+    return render(request, "minerals/minerals_list.html", {"minerals": minerals})
