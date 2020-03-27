d68413107993d08fd1fb90a9928de62d94d22285
Adam Kielar <47754270+adamkielar@users.noreply.github.com>
2020-03-27 17:30:05 +0100

    Deploy on PythonAnywhere

--- minerals_catalog/wsgi.py
+++ minerals_catalog/wsgi.py
@@ -8,9 +8,15 @@ https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
 """
 
 import os
+import sys
 
 from django.core.wsgi import get_wsgi_application
+from whitenoise.django import DjangoWhiteNoise
 
+path = "/home/adamkielar/minerals_catalog_v2/minerals_catalog"
+if path not in sys.path:
+        sys.path.append(path)
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'minerals_catalog.settings')
 
 application = get_wsgi_application()
+application = DjangoWhiteNoise(application)
